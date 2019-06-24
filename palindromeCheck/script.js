a5384adf709a459586b243a0518fb1bc3bedec21
unknown <grabowsky44@gmail.com>
2019-06-24 18:54:43 +0200

    Small update

---
+++ palindromeCheck/script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const form = document.querySelector(".form");
+    const wrapper = document.querySelector(".wrapper");
+    const moreInfo = document.querySelector(".wrapper__info");
+
+    moreInfo.addEventListener("click", () => {
+
+        let newParagraph = document.createElement("p");
+        newParagraph.classList.add("newElement");
+        let newParagraphText = document.createTextNode("A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward, such as madam or racecar or the number 10801.")
+        newParagraph.appendChild(newParagraphText);
+
+        newParagraph.style.padding = "1.5rem";
+        newParagraph.style.color = "#4286f4";
+        newParagraph.style.textTransform = "uppercase";
+        newParagraph.style.width = "50%";
+
+        let existCheck = document.querySelector(".newElement");
+        if(typeof(existCheck) != "undefined" && existCheck != null){
+            alert("Definition already exists.");
+        } else wrapper.insertBefore(newParagraph, form);
+    });
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let word = document.getElementById("word");
+        let reversed = word.value.split("").reverse().join("");
+        let checkPalindrome = (word.value === reversed) ? "This word is a palindrome." : "This word is not a palindrome.";
+
+        alert(checkPalindrome);
+        word.value = "";
+    });
+});
+
